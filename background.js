c2b3e98453e727861647d3f05d0c987ffd715f95
robinsonclo2 <rclower@cscc.edu>
2023-02-09 13:46:47 -0700

    Have ability to track if it should be on or off

--- background.js
+++ background.js
@@ -1,37 +1,65 @@
+console.log('hi')
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
     text: "OFF",
   });
-});
+  
+  chrome.storage.sync.get({
+    times: [],
+    onOff: []
+  }, function(items) {
+    items.times;
+    var today = new Date();
+    for (timeframe of items.onOff) {
+      var start = new Date();
+      var end = new Date();
+      start.setHours(timeframe[1].hour, timeframe[1].minute, 0);
+      end.setHours(timeframe[2].hour, timeframe[2].minute, 0);
 
-const extensions = 'https://developer.chrome.com/docs/extensions'
-const webstore = 'https://developer.chrome.com/docs/webstore'
-
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-
-    if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on
-      await chrome.scripting.insertCSS({
-        files: ["focus-mode.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ["focus-mode.css"],
-        target: { tabId: tab.id },
-      });
+      if (start.getTime() <= today.getTime() && end.getTime() > today.getTime()) {
+        console.log("currentTime", today)
+      }
     }
-  }
+    console.log(items.onOff)
+    } 
+  )
 });
+
+
+chrome.action.onClicked.addListener(function(tab) {
+  // console.log('clicked')
+  chrome.action.setBadgeText({
+    text: "on",
+  });
+  timeRemaining();
+});
+
+// function offOnCycle () {
+//   isOn = true;
+//   if (isOn) {
+//     chrome.action.setBadgeText({
+//       text: "on",
+//     });
+//   }
+// }
+
+function timeRemaining() {
+  // console.log('hi')
+  chrome.storage.sync.get({
+    times: [],
+    onOff: []
+  }, function(items) {
+    items.times;
+    var today = new Date();
+    for (timeframe of items.onOff) {
+      console.log(timeframe[1])
+      console.log(today.getTime())
+      if (timeframe[1].date >= today && timeframe[2].date < today) {
+        console.log("currentTime", today)
+      }
+    }
+    console.log(items.onOff)
+    } 
+  )
+} 
